c1ce8013466aaed5b771904c1a85303d383f2d5c
oussamah8 <ouscom2007@hotmail.com>
2020-04-18 00:04:13 +0100

    Updates

---
+++ precache-manifest.bddaf37340ee2d5cea254f95415cbff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbba44782776cec1ca0e169d6421e22a",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "9f9de8f4cd5217980d32",
+    "url": "/coronavirus/static/css/main.07fc0138.chunk.css"
+  },
+  {
+    "revision": "4bcd541dd5f909c470a1",
+    "url": "/coronavirus/static/js/2.847961e0.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/coronavirus/static/js/2.847961e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9de8f4cd5217980d32",
+    "url": "/coronavirus/static/js/main.9f796c7c.chunk.js"
+  },
+  {
+    "revision": "e1fdf3bb4b1b462ba31e",
+    "url": "/coronavirus/static/js/runtime-main.4f63cbcc.js"
+  }
+]);
